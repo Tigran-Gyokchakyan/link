efd25847f30e45c94f63cdadf6af76d393b7fe2c
tigran.t <tigrangyokchakyan93@gmail.com>
2022-09-09 18:29:22 +0400

    Change add compaign

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,12 +166,12 @@ $(function(){
 $("#search").on("keyup", function() {
     var value = $(this).val();
 
-    $("table tr").each(function(index) {
+    $(".togglechild").each(function(index) {
         if (index !== 0) {
 
             $row = $(this);
 
-            var id = $row.find("td:first").text();
+            var id = $row.find("td:nth-child(3)").text();
 
             if (id.indexOf(value) !== 0) {
                 $row.hide();
